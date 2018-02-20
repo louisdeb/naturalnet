982e3cb6f5afc65acf501940c24d8d9d5b13911f
Louis de Beaumont <debeaumont.louis@gmail.com>
2018-02-20 12:06:43 +0000

    Add main layout & display on app creation

--- app/src/main/java/com/louis/naturalnet/MainActivity.java
+++ app/src/main/java/com/louis/naturalnet/MainActivity.java
@@ -1,7 +1,25 @@
 package com.louis.naturalnet;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends Activity {
+
+    private String logTag = "NaturalNet:";
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(logTag,"Started main activity");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Set the default content view
+        setContentView(R.layout.main_layout);
+    }
 
 }
