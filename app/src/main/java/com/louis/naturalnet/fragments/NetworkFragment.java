43c5ca39c43c853e3509f0a263c9cefad7bbae5f
Louis de Beaumont <debeaumont.louis@gmail.com>
2018-02-21 09:22:22 +0000

    Add toggle for network status chevron on click

--- app/src/main/java/com/louis/naturalnet/fragments/NetworkFragment.java
+++ app/src/main/java/com/louis/naturalnet/fragments/NetworkFragment.java
@@ -5,10 +5,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import com.louis.naturalnet.R;
 
 public class NetworkFragment extends Fragment {
+
+    private boolean expanded = false;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -18,7 +21,11 @@ public class NetworkFragment extends Fragment {
         title.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // Perform expansion behaviour here
+                expanded = !expanded;
+
+                // Toggle chevron direction
+                view.findViewById(R.id.chevron_down_icon).setVisibility(expanded ? View.GONE : View.VISIBLE);
+                view.findViewById(R.id.chevron_up_icon).setVisibility(expanded ? View.VISIBLE : View.GONE);
             }
         });
 
