1aed95d680491d52ad74d8e0372cbef3fb3d047a
Louis de Beaumont <debeaumont.louis@gmail.com>
2018-03-01 09:00:04 +0000

    Click warning fragment to show example warning state

--- app/src/main/java/com/louis/naturalnet/fragments/WarningFragment.java
+++ app/src/main/java/com/louis/naturalnet/fragments/WarningFragment.java
@@ -5,13 +5,36 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import com.louis.naturalnet.R;
 
 public class WarningFragment extends Fragment {
 
+    private boolean expanded = false;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.warning_fragment, container, false);
+        final View view = inflater.inflate(R.layout.warning_fragment, container, false);
+
+        LinearLayout title = view.findViewById(R.id.warning_title_layout);
+        title.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View titleView) {
+                expanded = !expanded;
+
+                titleView.findViewById(R.id.warning_tick).setVisibility(expanded ? View.GONE : View.VISIBLE);
+                titleView.findViewById(R.id.warning_icon).setVisibility(expanded ? View.VISIBLE : View.GONE);
+
+                titleView.findViewById(R.id.no_warning_title).setVisibility(expanded ? View.GONE : View.VISIBLE);
+                titleView.findViewById(R.id.warning_title).setVisibility(expanded ? View.VISIBLE : View.GONE);
+
+                view.findViewById(R.id.warning_fragment_container).setBackgroundResource(
+                        expanded ? R.drawable.yellow_rounded_fragment : R.drawable.green_rounded_fragment);
+
+            }
+        });
+
+        return view;
     }
 
 }
