ac17ec04b9d6344095e51b8599be33afd0b458e5
Louis de Beaumont <debeaumont.louis@gmail.com>
2018-05-19 11:43:48 +0100

    Add azimuth algorithm

--- app/src/main/java/com/louis/naturalnet/device/NaturalNetDevice.java
+++ app/src/main/java/com/louis/naturalnet/device/NaturalNetDevice.java
@@ -56,9 +56,13 @@ public class NaturalNetDevice {
         return score;
     }
 
+    // The default score this function returns is 2 * EARTH_DIAMETER.
+    // For a distance x between the device and the phone, score += EARTH_DIAMETER - x
+    // For a time t to reach the zone, score += t
+    // If we don't have a estimated time, score += EARTH_DIAMETER (i.e. a penalty)
     private double getLocationScore(JSONObject destination) throws JSONException {
         if (destination == null || location == null)
-            return Constants.EARTH_DIAMETER;
+            return 2 * Constants.EARTH_DIAMETER;
 
         double score;
         double lat = location.getLatitude();
@@ -83,47 +87,63 @@ public class NaturalNetDevice {
         */
 
         double destLat1 = destination.getDouble(Warning.WARNING_LAT_START);
-        double destLon1 = destination.getDouble(Warning.WARNING_LON_START);
         double destLat2 = destination.getDouble(Warning.WARNING_LAT_END);
+        double destLon1 = destination.getDouble(Warning.WARNING_LON_START);
         double destLon2 = destination.getDouble(Warning.WARNING_LON_END);
 
-        Log.d(TAG, "Calculating distance between location " + lat + ", " + lon + " and zone (" + destLat1 + ", " +
-                          destLon1 + ") (" + destLat2 + ", " + destLon2 + ")");
+        // Use the centre location of the zone as our destination point.
+        double destLat = (destLat1 + destLat2) / 2;
+        double destLon = (destLon1 + destLon2) / 2;
 
         // Calculate the smallest distance between the device and the location area.
-        double diffLon = Math.min(Math.toRadians(Math.abs(destLon1 - lon)),
-                                  Math.toRadians(Math.abs(destLon2 - lon)));
-
-        Log.d(TAG, "diffLon: " + diffLon);
-
-        double minDistance = Math.min(haversine(lat, destLat1, diffLon),
-                                      haversine(lat, destLat2, diffLon));
-
+        double minDistance = haversine(lat, lon, destLat, destLon);
         Log.d(TAG, "minDistance: " + minDistance);
 
         score = Constants.EARTH_DIAMETER - minDistance;
-
         Log.d(TAG, "score: " + score);
 
         // If we have movement information, calculate how soon this device will reach the target location.
         if (location.hasBearing() && location.hasSpeed()) {
             // Calculate time till arrival and add it on to the score
             // If the device is travelling away from the destination we'd want to take away some amount from the score.
+            double bearing = location.getBearing();
+            double speed = location.getSpeed();
+
+            double azimuth = Math.toDegrees(azimuth(lat, lon, destLat, destLon));
+
+            // We want to calculate whether travelling along the bearing from lat,lon will put us in the destination zone.
+        } else {
+            score += Constants.EARTH_DIAMETER;
         }
 
         return score;
     }
 
-    private double haversine(double lat, double destLat, double diffLon) {
-        double slat = Math.toRadians(lat);
-        double flat = Math.toRadians(destLat);
+    // An implementation of the Haversine formula, which computes the great-circle distance between two points on a
+    // sphere. https://www.movable-type.co.uk/scripts/latlong.html
+    private double haversine(double lat1, double lon1, double lat2, double lon2) {
+        double phi1 = Math.toRadians(lat1);
+        double phi2 = Math.toRadians(lat2);
 
-        double diffLat = Math.toRadians(Math.abs(destLat - lat));
+        double diffLat = Math.toRadians(Math.abs(lat2 - lat1));
+        double diffLon = Math.toRadians(Math.abs(lon2 - lon1));
+
         double a = Math.pow(Math.sin(diffLat / 2), 2) +
-                   Math.cos(slat) * Math.cos(flat) * Math.pow(Math.sin(diffLon / 2), 2);
+                   Math.cos(phi1) * Math.cos(phi2) * Math.pow(Math.sin(diffLon / 2), 2);
         double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 
         return Constants.EARTH_DIAMETER * c;
+    }
+
+    // Returns the initial bearing (in radians) of the line from lat1,lon1 to lat2,lon2.
+    // https://www.movable-type.co.uk/scripts/latlong.html
+    private double azimuth(double lat1, double lon1, double lat2, double lon2) {
+        double phi1 = Math.toRadians(lat1);
+        double phi2 = Math.toRadians(lat2);
+        double diffLon = Math.toRadians(Math.abs(lon2 - lon1));
+
+        return Math.atan2(Math.sin(diffLon) * Math.cos(phi2),
+                          Math.cos(phi1) * Math.sin(phi2) - Math.sin(phi1) * Math.cos(phi2) * Math.cos(diffLon));
     }
 
 }
